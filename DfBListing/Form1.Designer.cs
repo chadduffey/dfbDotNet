10ef4fb2f20e2724225718a199a0b9f822f843c9
Chad Duffey <chadduffey@hotmail.com>
2015-07-27 16:30:03 +1000

    git the json deserialize working

---
+++ DfBListing/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DfBListing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtboxToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(15, 150);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(221, 176);
+            this.treeView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(611, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(244, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(540, 125);
+            this.textBox1.TabIndex = 2;
+            // 
+            // txtboxToken
+            // 
+            this.txtboxToken.Location = new System.Drawing.Point(10, 25);
+            this.txtboxToken.Name = "txtboxToken";
+            this.txtboxToken.Size = new System.Drawing.Size(590, 20);
+            this.txtboxToken.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dropbox for Business Admin Token:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(10, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 470);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtboxToken);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Dropbox for Business";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtboxToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ DfBListing/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Web;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+
+namespace DfBListing
+{
+  
+    [DataContract]
+    class Profile
+    {
+        //profile:
+        [DataMember]
+        public string given_name { get; set; }
+        [DataMember]
+        public string surname { get; set; }
+        [DataMember]
+        public string status { get; set; }
+        [DataMember]
+        public string member_id { get; set; }
+        [DataMember]
+        public string email { get; set; }
+        [DataMember]
+        public string external_id { get; set; }
+        [DataMember]
+        public List<string> groups { get; set; }
+    }
+    
+    [DataContract]
+    class Permissions
+    {
+        //permissions:
+        [DataMember]
+        public bool is_admin { get; set; }
+    }
+
+    [DataContract]
+    class Member
+    //member is made up of profile and permissions components
+    {
+        [DataMember]
+        public Profile profile { get; set; }
+        [DataMember]
+        public Permissions permissions { get; set; }
+    }
+
+    [DataContract]
+    class Team
+    {
+        //team is made up of members and has a cursor and has_more attribute
+        [DataMember]
+        public List<Member> members { get; set; }
+        [DataMember]
+        public string cursor { get; set; }
+        [DataMember]
+        public bool has_more { get; set; } 
+    }
+
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtboxToken.Text = "32Wp8V0dZ28AAAAAAACiCt2gcRLGDd-2IeDXLTuS3sV6NiQSCucUZ5u8RFECzRdQ";
+            
+            //
+            // This is the first node in the view.
+            //
+            TreeNode treeNode = new TreeNode("Windows");
+            treeView1.Nodes.Add(treeNode);
+            //
+            // Another node following the first node.
+            //
+            treeNode = new TreeNode("Linux");
+            treeView1.Nodes.Add(treeNode);
+            //
+            // Create two child nodes and put them in an array.
+            // ... Add the third node, and specify these as its children.
+            //
+            TreeNode node2 = new TreeNode("C#");
+            TreeNode node3 = new TreeNode("VB.NET");
+            TreeNode[] array = new TreeNode[] { node2, node3 };
+            //
+            // Final node.
+            //
+            treeNode = new TreeNode("Dot Net Perls", array);
+            treeView1.Nodes.Add(treeNode);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = txtboxToken.Text;
+            comboBox1.Items.Add("Test");
+            comboBox1.Items.Add("Second Test");
+
+            //call the API
+            string testtext = dfb_members_list();
+
+            textBox1.Text = testtext;
+
+            //Dictionary<string, string>[] members = JsonConvert.DeserializeObject<Dictionary<string, string>[]>(testtext);
+            
+            //System.Diagnostics.Debug.WriteLine(members[0]["surname"]);
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Team));
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(testtext));
+            Team obj = (Team)ser.ReadObject(stream);
+
+            
+        }
+
+        private string dfb_members_list()
+        {
+            String page = "https://api.dropbox.com/1/team/members/list";
+            WebRequest request = WebRequest.Create(page);
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            String authheader = "Authorization:Bearer " + txtboxToken.Text;
+            request.Headers.Add(authheader);
+
+            // Create POST data and convert it to a byte array.
+            string postData = "{}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream ds = request.GetRequestStream();
+
+            // Write the data to the request stream.
+            ds.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            ds.Close();
+
+            // Get the response.
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Display the status.
+                Console.WriteLine(response.StatusDescription);
+
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content. 
+                string responseFromServer = reader.ReadToEnd();
+
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                return responseFromServer;
+            }
+
+
+            catch (WebException error)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.WriteLine("We hit a snag: " + error);
+                return "We hit an error in basicAPIcall()";
+            }
+
+        
+        }
+
+
+
+
+
+
+
+
+
+    //class - Form1
+    }
+
+//namespace DfBListing
+}
